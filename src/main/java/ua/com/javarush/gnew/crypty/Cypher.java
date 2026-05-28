d8d201636691869874f64accf58df495d686830a
oleksandr-jr <oleksandr.cho@gmail.com>
2026-05-28 19:32:40 +0200

    Implemented decrypt and improved encrypt.

--- src/main/java/ua/com/javarush/gnew/crypty/Cypher.java
+++ src/main/java/ua/com/javarush/gnew/crypty/Cypher.java
@@ -2,8 +2,14 @@ package ua.com.javarush.gnew.crypty;
 
 public class Cypher {
 
-  public String encrypt() {
+  public String encrypt(String text, int key) {
 
     return "";
   }
+
+  public String decrypt(String text, int key) {
+
+    return "";
+  }
+
 }
