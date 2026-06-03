2b4a974ce74d2d1af3b8752f102d6bd60f769fde
misha <misakadanec102@gmail.com>
2026-06-03 18:15:37 +0300

    Update Cipher and create class Alphabet

--- src/main/java/ua/com/javarush/j4/Cipher.java
+++ src/main/java/ua/com/javarush/j4/Cipher.java
@@ -3,30 +3,24 @@ package ua.com.javarush.j4;
 import java.util.ArrayList;
 import java.util.Collections;
 
-public class Cipher {
-    public String encrypt(String text, String key, ArrayList<Character> alphabet) {
-        ArrayList <Character>newAlphabet = new ArrayList(alphabet);
-        int k = Integer.parseInt(key);
-        Collections.rotate(newAlphabet,k );
-        char[] chars = text.toCharArray();
-        StringBuilder result = new StringBuilder();
-        for(char c : chars) {
-            if(alphabet.contains(c)) {
+public class Cipher  {
+    public String encrypt(String text, int key, ArrayList<Character> alphabet) {
+        ArrayList <Character>newAlphabet = new ArrayList<>(alphabet);
 
-                int index = alphabet.indexOf(c);
-                result.append(newAlphabet.get(index));
-            }else  {
-                result.append(c);
-            }
-        }
-        return result.toString();
+        Collections.rotate(newAlphabet,-key );
+        char[] chars = text.toCharArray();
+        return cycle(chars,alphabet,newAlphabet);
     }
 
-    public String decrypt(String text, String key, ArrayList<Character> alphabet) {
-        ArrayList <Character>newAlphabet = new ArrayList(alphabet);
-        int k = (Integer.parseInt(key))*(-1);
-        Collections.rotate(newAlphabet,k );
+    public String decrypt(String text, int key, ArrayList<Character> alphabet) {
+        ArrayList <Character> newAlphabet = new ArrayList<>(alphabet);
+
+        Collections.rotate(newAlphabet,key );
         char[] chars = text.toCharArray();
+
+        return cycle(chars,alphabet,newAlphabet);
+    }
+    public String cycle(char[] chars ,ArrayList<Character> alphabet,ArrayList<Character> newAlphabet) {
         StringBuilder result = new StringBuilder();
         for(char c : chars) {
             if(alphabet.contains(c)) {
--- src/main/java/ua/com/javarush/j4/Main.java
+++ src/main/java/ua/com/javarush/j4/Main.java
@@ -2,8 +2,7 @@ package ua.com.javarush.j4;
 
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
+
 
 /**
  * Точка входу криптоаналізатора шифру Цезаря.
@@ -15,22 +14,23 @@ import java.util.List;
 public class Main {
     public static void main(String[] args) throws IOException {
         // TODO: реалізуй CLI шифру Цезаря. Дивись MainTest.
-        String kay = "5";
+        int kay = 1;
 
         FileReader file = new FileReader("C:\\Users\\user\\IdeaProjects\\M1-FP-Mykhailo\\src\\main\\resources\\input.txt");
         String text = file.readAllAsString();
-        String text2 = "Hello World!";
-          ArrayList<Character> englishAlphabet = new ArrayList<>(List.of(
-                'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
-                'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
-                'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
-                'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
-                '.', ',', '«', '»', '"', '\'', ':', '!', '?'
-        ));
+        String text2 = "АААААВВВВ";
+//          ArrayList<Character> englishAlphabet = new ArrayList<>(List.of(
+//                'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
+//                'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
+//                'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
+//                'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
+//                '.', ',', '«', '»', '"', '\'', ':', '!', '?'
+//        ));
           Cipher cipher = new Cipher();
 
-        System.out.println(cipher.encrypt(text, kay, englishAlphabet));
-
+        System.out.println(cipher.encrypt(text2, kay,Alphabet.getLanguishes(text2)));
+        String newText = cipher.encrypt(text2, kay, Alphabet.UKR.getAlphabet());
+        System.out.println(cipher.decrypt(newText, kay, Alphabet.UKR.getAlphabet()));
 
 
 
