f068bc5ed7527b3c59166fad465494190ec68623
oleksandr-jr <oleksandr.cho@gmail.com>
2026-06-08 19:28:10 +0200

    Cypher

--- src/main/java/ua/com/javarush/gnew/crypty/Cypher.java
+++ src/main/java/ua/com/javarush/gnew/crypty/Cypher.java
@@ -1,15 +1,28 @@
 package ua.com.javarush.gnew.crypty;
 
+import ua.com.javarush.gnew.language.Language;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class Cypher {
 
-  public String encrypt(String text, int key) {
+  public String encrypt(String text, int key, Language language) {
+    List<Character> original = language.alphabet();
+    List<Character> rotated = new ArrayList<>(original);
+    Collections.rotate(rotated, Math.negateExact(key));
 
-    return text + key;
+    StringBuilder out = new StringBuilder(text.length());
+    for (int i = 0; i < text.length(); i++) {
+      char c = text.charAt(i);
+      int idx = original.indexOf(c);
+      out.append(idx < 0 ? c : rotated.get(idx));
+    }
+    return out.toString();
   }
 
-  public String decrypt(String text, int key) {
-
-    return "";
+  public String decrypt(String text, int key, Language language) {
+    return encrypt(text, -key, language);
   }
-
 }
